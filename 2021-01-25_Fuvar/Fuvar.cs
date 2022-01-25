bbed69f5813029928b3fb46c50f638001449cc23
palinkaspeter <palinkasp.12d@acsjszki.hu>
2022-01-25 13:12:17 +0100

    Add class Fuvar

---
+++ 2021-01-25_Fuvar/Fuvar.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2021_01_25_Fuvar
+{
+    class Fuvar
+    {
+        public int TaxiID { get; set; }
+        public string Indulás { get; set; }
+        public int Időtartam { get; set; }
+        public double Távolság { get; set; }
+        public double VitelDíj { get; set; }
+        public double Borravaló { get; set; }
+        public string FizetésMód { get; set; }
+
+        public Fuvar(string sor)
+        {
+            string[] t = sor.Split(';');
+            TaxiID = int.Parse(t[0]);
+            Indulás = t[1];
+            Időtartam = int.Parse(t[2]);
+            Távolság = double.Parse(t[3]);
+            VitelDíj = double.Parse(t[4]);
+            Borravaló = double.Parse(t[5]);
+            FizetésMód = t[6];
+        }
+    }
+}
